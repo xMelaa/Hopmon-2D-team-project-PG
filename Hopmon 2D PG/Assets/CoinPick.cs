1d19a539cfc32d79134c2d67cfc78c15edd43e4d
kevinbienias <kevinbienias1@gmail.com>
2021-04-21 22:37:29 +0200

    Silnik 0.07
    -dodano pierwszą mapę
    -dodano muzykę
    -dodano Coins
    -dodano TextMeshPro
    SKRYPTY:
    nowe:
    -CoinPick (z komentarzami)

---
+++ Hopmon 2D PG/Assets/CoinPick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class CoinPick : MonoBehaviour
+{
+    //Aktualna ilość punktów
+    private int coin = 0;
+    //Pobieramy muzykę do coinSound
+    public AudioClip coinSound;
+    //Głośność muzyki
+    public float volume;
+
+    //Pobieramy TextMeshPro do textCoins
+    public TextMeshProUGUI textCoins;
+    //W trakcie kolizji
+         private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coins")
+        {
+            //powiększ coin o 1
+            coin++;
+            //zmien wczesniej wybrany TextMeshPro na Tekst + ilość punktów zamienionych na string        
+            textCoins.text = "Punkty: " + coin.ToString();
+            //TO BĘDZIE DO ZMIANY
+            if(coin==2)
+            {
+               textCoins.text = "MAX PUNKTÓW"; 
+            }
+            //Odtwórz dźwięk coinSound w miejscu 0,0,0 z głośnościa volume
+            AudioSource.PlayClipAtPoint(coinSound, Vector3.zero, volume);
+            //Zniszcz obiekt
+            Destroy(collision.gameObject);
+        }
+ 
+    }
+}

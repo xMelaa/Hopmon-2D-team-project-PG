088b477cb3f0d43a593795f81ed7ef5ab2c491e3
joannakwiat <62302596+joannakwiat@users.noreply.github.com>
2021-04-23 09:49:53 +0200

    Komentarz do menu.cs

--- Hopmon 2D PG/Assets/Menu.cs
+++ Hopmon 2D PG/Assets/Menu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+	//index 0 = menu
+	//index 1 = gra (lvl 1)
+	//przeskok między scenami dzięki inkrementacji indexu
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }

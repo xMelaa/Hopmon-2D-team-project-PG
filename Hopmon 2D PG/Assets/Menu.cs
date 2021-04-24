ae7ffc8d04df172c5bcdc1a208f8241f6375656a
kevinbienias <kevinbienias1@gmail.com>
2021-04-24 23:19:16 +0200

    2d oświetlenie
    + poprawa pause menu

--- Hopmon 2D PG/Assets/Menu.cs
+++ Hopmon 2D PG/Assets/Menu.cs
@@ -5,16 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    //index 0 = menu
-    //index 1 = gra (lvl 1)
-    //przeskok między scenami dzięki inkrementacji indexu
-    public void PlayGame()
-    {
+	//index 0 = menu
+	//index 1 = gra (lvl 1)
+	//przeskok między scenami dzięki inkrementacji indexu
+    public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame()
-    {
+    public void QuitGame(){
         Application.Quit();
     }
 }
--- Hopmon 2D PG/Assets/PlayerMovement.cs
+++ Hopmon 2D PG/Assets/PlayerMovement.cs
@@ -5,43 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public bool isPaused;
-    public GameObject pauseMenu;
-    public Animator animator;
-
-    public float moveSpeed = 5f;
-
+    public float moveSpeed;
     public Rigidbody2D rb;
-    public Camera cam;
-
-    private Vector2 movement;
-    private Vector2 mousePos;
-    private Vector2 lookDir;
+    public Animator animator;
+    public GameObject pauseMenu;
+    public bool isPaused;
+    private Vector2 moveDirection;
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-
-        //moveDirection = new Vector2(moveX, moveY).normalized;
-        //animator.SetFloat("Horizontal", lookDir.x);
-        //animator.SetFloat("Vertical", lookDir.y);
-        //animator.SetFloat("Speed", lookDir.sqrMagnitude);
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vertical", moveDirection.y);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
 
         //reakcja na escape - włączenie/wyłączenie pauzy
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
-            {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
                 pauseMenu.SetActive(false);
-                Time.timeScale = 1f;
+                Time.timeScale = 1f; 
                 isPaused = false;
             }
-            else
-            {
+            else {
                 pauseMenu.SetActive(true);
                 Time.timeScale = 0f; //zatrzymaj czas
                 isPaused = true;
@@ -49,18 +38,13 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void BackToMenu()
-    { //potrzebne do okna pauzy, skok do menu
+    public void BackToMenu(){ //potrzebne do okna pauzy, skok do menu
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1f;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        //rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }

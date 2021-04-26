04162b437354dc4efce808573c6271cfb05e7dab
Milena Kierys <melaka.mk@gmail.com>
2021-04-26 16:34:10 +0200

    Teleport
    Gdy stanie sie na zlotym to  teleportuje na kolejna scene (level0

---
+++ Hopmon 2D PG/Assets/Teleport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public PlayerMovement player;
+    public string nextLvl;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement> ();
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D CoTo)
+    {
+        if(CoTo.name== "Player"){
+            SceneManager.LoadScene(nextLvl);
+    }
+    }
+}
